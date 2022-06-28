93d95b0b67e26522e4ca498189192c44f1927220
matheus-96 <victor.id96@hotmail.com>
2022-06-28 15:06:31 -0300

    alteração na listagem de usuário para deletar user

--- frontend/ts/users/listUser.ts
+++ frontend/ts/users/listUser.ts
@@ -1,3 +1,7 @@
+import { toggleModal, initModal } from "../common/modal.js"
+
+initModal('modal')
+
 var users: Array<Object>
 var containerUsers = document.querySelector('.container-users') as HTMLDivElement
 
@@ -9,26 +13,52 @@ window.addEventListener('load', async () => {
 
 })
 
-document.querySelector('.footer')?.addEventListener('click', (event)=> {
+containerUsers.addEventListener('click', event => {
+    let target = event.target as HTMLElement
+    
+    if (target.classList.contains('deleteBtn')) {
+        let deleteId = document.querySelector('#deleteId') as HTMLInputElement
+        let id = target.dataset.id
+        if (id) deleteId.value = id
+        toggleModal('modal')
+
+    }
+})
+
+document.querySelector('.modal')?.addEventListener('click', event => {
+    let target = event.target as HTMLElement
+    if(target.classList.contains('delete')){
+        let idInput = document.querySelector('#deleteId') as HTMLInputElement
+        let id = idInput.value
+        deleteUser(id)
+    }
+})
+
+document.querySelector('.footer')?.addEventListener('click', (event) => {
     let clickedElement = event.target as HTMLButtonElement
-    if(clickedElement.tagName == "BUTTON"){
-        
+    if (clickedElement.tagName == "BUTTON") {
+
         let page = clickedElement.dataset.page!
 
         page = page
         updateCards(page)
         console.log(page);
-        
+
     }
 })
 
+async function deleteUser(id: string) {
+    let response = await fetch(`http://127.0.0.1:3000/api/v1/users/${id}`, { method: 'DELETE' })
+    users = await response.json()
+}
+
 async function getUsers() {
-    let response = await fetch('http://127.0.0.1:3000/api/v1/users', {method: 'GET'})
+    let response = await fetch('http://127.0.0.1:3000/api/v1/users', { method: 'GET' })
     users = await response.json()
 }
 
 async function updateCards(page: string) {
-    if(containerUsers){
+    if (containerUsers) {
         containerUsers.innerHTML = ''
         users[page].map(el => {
             containerUsers.innerHTML += createCard(el);
@@ -36,67 +66,74 @@ async function updateCards(page: string) {
     }
 }
 
-function createCard(user: Object){
-return `
-<a href='./listUserId.html?id=${user['_id']}'
-    <div class="card">
-    <div class="">
-        <label for="">Name</label>
-        <p class="name">
-            ${user['name']}
-        </p>
-        <div class="div"></div>
-        <label for="">E-mail</label>
-        <p class="email">
-        ${user['email']}
-        </p>
-    </div>
-    <div class="">
-        <label for="">Birth Date</label>
-        <p class="birthDate">
-        ${user['birthDate']}
-        </p>
-    </div>
-    <div class="">
-        <div class="">
-            <label for="">City</label>
-            <p class="city">
-            ${user['city']}
-            </p>
-        </div>
-        <div class="">
-            <label for="">State</label>
-            <p class="state">
-            ${user['state']}
-            </p>
-        </div>
-    </div>
-    <div class="">
-        <label for="">Address</label>
-        <p class="address">
-        ${user['address']}
-        </p>
-        <label for="">Number</label>
-        <p class="number">
-        ${user['number']}
-        </p>
-    </div>
+function createCard(user: Object) {
+    return `
+<div class="card">
+                        <div class="div">
+                            <label for="">Name</label>
+                            <p class="name">
+                                ${user['name']}
+                            </p>
+                            <div class="div"></div>
+                            <label for="">E-mail</label>
+                            <p class="email">
+                                ${user['email']}
+                            </p>
+                        </div>
+                        <div class="div">
+                            <label for="">Birth Date</label>
+                            <p class="birthDate">
+                                ${user['birthDate']}
+                            </p>
+                        </div>
+                        <div class="div">
+                            <div class="">
+                                <label for="">City</label>
+                                <p class="city">
+                                    ${user['city']}
+                                </p>
+                            </div>
+                            <div class="">
+                                <label for="">State</label>
+                                <p class="state">
+                                    ${user['state']}
+                                </p>
+                            </div>
+                        </div>
+                        <div class="div">
+                            <label for="">Address</label>
+                            <p class="address">
+                                ${user['address']}
+                            </p>
+                            <label for="">Number</label>
+                            <p class="number">
+                                ${user['number']}
+                            </p>
+                        </div>
+                    </a>
+                    <div class="">
+                        <p>
+                            <a href='./listUserId.html?id=${user['_id']}'><button class="btn top">Edit</button></a>
+                        </p>
+                        <p>
+                            <button class="btn bottom deleteBtn" data-id='${user['_id']}'>Delete</button>
+                        </p>
+                    </div>
 
-</div>
-</a>`
+                </div>`
 }
 
 function updateButtons() {
     let footer = document.querySelector('footer')
-    if(footer){
+    if (footer) {
         footer.innerHTML = ''
-                
+
         for (let x = 0; x < users.length; x++) {
             let b = document.createElement('button') as HTMLButtonElement
             b.dataset.page = x + ""
-            b.textContent = (x + 1)+""
+            b.textContent = (x + 1) + ""
             footer.append(b)
-            
+
         }
     }
 }
